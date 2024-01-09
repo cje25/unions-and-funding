ee1f3bb658e2dcfeffec502a37eee1b37f83b774
Cameron Eller <ce1006a@student.american.edu>
2024-01-09 18:47:07 -0500

    cleaned the names of the congresspeople and encoded names using latin1

--- data/01-union_score_cleaning.R
+++ data/01-union_score_cleaning.R
@@ -4,12 +4,16 @@ library(tidyverse)
 
 
 # importing the union score data for the house candidates:
-house_scores_df <- read_csv(here("final_project/unions-and-funding/data", "AFL-CIO-Scorecard-2022-House.csv"))
-house_scores_df <- mutate(house_scores_df, chamber = "House")
+house_scores_df <- read.csv(here("data", "AFL-CIO-Scorecard-2022-House.csv"), 
+                            encoding = "latin1")
+house_scores_df <- mutate(house_scores_df, chamber = "House") |>
+                      mutate(Name = sub('Rep. ', '', Name))
 
 # importing the union score data for the senate candidates:
-senate_scores_df <- read_csv(here("final_project/unions-and-funding/data", "AFL-CIO-Scorecard-2022-Senate.csv"))
-senate_scores_df <- mutate(senate_scores_df, chamber = "Senate")
+senate_scores_df <- read.csv(here("data", "AFL-CIO-Scorecard-2022-Senate.csv"),
+                             encoding = "latin1")
+senate_scores_df <- mutate(senate_scores_df, chamber = "Senate") |>
+                      mutate(Name = sub('Sen. ', '', Name))
 
 union_scores_df <- bind_rows(house_scores_df, senate_scores_df)
 
@@ -19,7 +23,7 @@ union_scores_df <- union_scores_df |>
                              "District", 
                              "Party", 
                              "Score", 
-                             "Lifetime Score", 
+                             "Lifetime.Score", 
                              "chamber") |>
                       rename(
                         name = Name,
@@ -27,12 +31,19 @@ union_scores_df <- union_scores_df |>
                         district = District,
                         party = Party,
                         score = Score,
-                        lifetime_score = "Lifetime Score"
+                        lifetime_score = "Lifetime.Score"
                       ) |>
                       mutate(score = as.numeric(gsub('%', '', score)),
                              lifetime_score = as.numeric(gsub('%', '', lifetime_score)))
 
 union_scores_df
+
+other_par <- 'ID'
+test_par <- c('Republican', 'Democrat')
+
+independent_candidates <- union_scores_df |> filter(union_scores_df$party %in% other_par)
+
+
 
 
 write.csv(union_scores_df, "cleaned_union_scores.csv", row.names=FALSE)
